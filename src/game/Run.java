db51cf205967c4dfca25f53785a1c74a31a5c0a5
nickgg11 <nicholas.gingras@ocsbstudent.ca>
2017-05-31 13:19:55 -0400

    Begining the card choice section

--- src/game/Run.java
+++ src/game/Run.java
@@ -5,6 +5,7 @@ public class Run {
 	public static void main(String[] args) {
 		//VARIABLES
 		Card[] deck=new Card[40];
+		Card[] discard=new Card[40];
 		Fluster[] flusterDeck=new Fluster[13];
 		Card[] player1=new Card[5];
 		Card[] player2=new Card[5];
@@ -14,7 +15,7 @@ public class Run {
 		
 		//MAIN
 		Setup(deck,flusterDeck);
-		Deal(deck, player1, player2, player3, player4);
+		Deal(deck, player1, player2, player3, player4, discard);
 		System.out.println("Your cards are: ");
 		for (int x=0;x<player1.length;x++){
 			System.out.println("Card "+(x+1)+": "+player1[x].getValue()+" of "+player1[x].getSuit());
@@ -26,7 +27,18 @@ public class Run {
 		}while(choice<1||choice>5);
 		System.out.println("You picked "+player1[choice-1].getValue()+" of "+player1[choice-1].getSuit());
 		
+		System.out.println("Okay would like to draw from the discard pile or from the deck");
+		System.out.println("the card on the top of the discard pile is: "+discard);
+		System.out.println("1: Deck\n2: Discard pile");
+		int drawChoice=in.nextInt();
+		if (drawChoice==1)
+		{
+			System.out.println("The card drawn is");
+			System.out.println("Do you want to\nDiscard this card: 1\nkeep this card and discard another: 2");
+		}
+		Card tempCard = player1[choice];
 	}
+	
 	//HELPER METHODS
 
 	//SETUP
@@ -58,12 +70,13 @@ public class Run {
 	}
 	
 	//DEAL
-	public static void Deal(Card[] deck, Card[] p1, Card[] p2,Card[] p3, Card[] p4){
+	public static void Deal(Card[] deck, Card[] p1, Card[] p2,Card[] p3, Card[] p4, Card[] discard){
 		for (int x=0;x<5;x++){
 			p1[x]=deck[x*4];
 			p2[x]=deck[x*4+1];
 			p3[x]=deck[x*4+2];
 			p4[x]=deck[x*4+3];
 		}
+		discard[0]=deck[24];
 	}
 }
